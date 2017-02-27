f84934c04310cbc2016625c11eb69be2429c5221
Meije Gawinowski <meije.gawinowski@wanadoo.fr>
2017-02-27 17:55:54 +0100

    Mutation improvement and indentation correction

--- SCell.cpp
+++ SCell.cpp
@@ -36,22 +36,19 @@ SCell::~SCell(){
 //    PUBLIC METHODS
 //==============================
 
-float SCell::Fitness()
-{
-	if (p_[2] < WMIN_)
-	{
-		return 0;
-	}
-	else{
-		return p_[2];
-	}
+float SCell::Fitness(){
+  if (p_[2] < WMIN_){
+    return 0;
+  }
+  else{
+    return p_[2];
+  }
 }
 
 vector<float> SCell::Metabolism(vector<float>* conc, float t){
-	conc->at(1) += -t*conc->at(1)*Rbb_;
-	p_.at(1) += t*(conc->at(1)*Rbb_-p_.at(1)*Rbc_);
-	p_.at(2) += t*conc->at(1)*Rbc_;
-	w_ = Fitness();
-
-	return *conc;
+  conc->at(1) += -t*conc->at(1)*Rbb_;
+  p_.at(1) += t*(conc->at(1)*Rbb_-p_.at(1)*Rbc_);
+  p_.at(2) += t*conc->at(1)*Rbc_;
+  w_ = Fitness();
+  return *conc;
 }
